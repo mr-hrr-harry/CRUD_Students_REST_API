470a72b8f9db34927243aefaf1c77686b7179a7e
M A HARISH J <mr.horror3737@gmail.com>
2023-06-05 21:01:33 +0530

    last commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1/HarryDB'
+const url = 'mongodb://127.0.0.1:27017/HarryDB'
 
 const app = express()
 
