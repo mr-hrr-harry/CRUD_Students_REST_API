cae9091c7b6e973bbd933293c38ff636969fc8f2
M A HARISH J <mr.horror3737@gmail.com>
2023-06-16 21:13:15 +0530

    env log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1:27017/HarryDB'
+const dotenv = require('dotenv').config()
+const url = "mongodb://127.0.0.1:27017/HarryDB"
 
 const app = express()
 
@@ -19,7 +20,7 @@ app.use(express.json())
 const homeRouter = require('./routers/home')
 app.use('/home', homeRouter)
 
-app.listen(3737, () => {                // Callback as arrow function
+app.listen(process.env.port, () => {                // Callback as arrow function
     console.log('Server Started!')
 })
 
