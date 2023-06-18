078154d9ab74ad061786792ea5220c1df9c29752
M A HARISH J <mr.horror3737@gmail.com>
2023-06-18 19:51:23 +0530

    db atlas cmmt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
-const url = "mongodb://127.0.0.1:27017/HarryDB"
+const url = process.env.url
 
 const app = express()
 
